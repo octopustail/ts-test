ff3d6fa56d06cfccaeff22a51ecc1ade3040c8fe
Vale Zhang <494479914@qq.com>
2024-05-06 15:13:51 +1000

    feat(get-arnie-quotes): my accepted answer

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,39 @@
 import { httpGet } from './mock-http-interface';
 
-// TODO define this type properly
-type TResult = any;
+enum ResultKey {
+  Success = 'Arnie Quote',
+  Fail = 'FAILURE'
+}
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+enum PromiseStatus {
+  Fulfill = 'fulfilled',
+  Rejected = 'rejected'
+}
+
+type TResult = Record<ResultKey.Success, string> | Record<ResultKey.Fail, string>;
+
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+  try {
+    const requests = urls.map(url => {
+      return httpGet(url);
+    });
+    const responses = await Promise.allSettled(requests)
+    return responses.map(result => {
+      if (result.status === PromiseStatus.Rejected) {
+        return { [ResultKey.Fail]: result.reason };
+      }
+      const { status, body } = result.value;
+      const { message } = JSON.parse(body);
+
+      if (status === 200) {
+        return { [ResultKey.Success]: message };
+      }
+      return { [ResultKey.Fail]: message };
+    })
+  } catch (error) {
+    // in production, do not show console.
+    console.log('Error occurs while fetching Arnie quotes', error);
+    // in case no error handler in caller, make sure return a array.
+    return [];
+  }
 };
